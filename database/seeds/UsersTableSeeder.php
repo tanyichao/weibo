653e1857a4f922dc6ae14b940d9c35af8112ecdd
vanman <tanyichao110@163.com>
2020-05-08 04:24:28 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'tanyichao';
         $user->email = '457345111@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
